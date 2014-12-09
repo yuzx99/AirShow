19a02a4e08a64a6ad6d14dea375399a079858716
Keller Wang <frank092806@gmail.com>
2014-12-09 13:58:33 +0800

    get the advertising of main program and show it; add the xUtils jar.

--- src/com/mircobox/airshow/SplashActivity.java
+++ src/com/mircobox/airshow/SplashActivity.java
@@ -1,21 +1,52 @@
 package com.mircobox.airshow;
 
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.lidroid.xutils.BitmapUtils;
+import com.mircobox.model.*;
 
 public class SplashActivity extends Activity {
 
 	private Timer timer;
 	private long waitTime = 3000;
 	private long touchTime = 0;
+	private String iamgeName = "mockimage.jpeg";
+	private String urlMockimage = "http://10.60.43.10:5000/news/mockimage";
+	private LinearLayout llbg = null;
+
+	// Handler handlerBackground = new Handler() {
+	// @Override
+	// public void handleMessage(Message msg) {
+	// super.handleMessage(msg);
+	// Bundle data = msg.getData();
+	// String dir = data.getString("dir");
+	// String filePath = dir + "/" + iamgeName;
+	// Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+	// BitmapDrawable bDrawable = new BitmapDrawable(bitmap);
+	// // set advertisement page background
+	// llbg.setBackgroundDrawable(bDrawable);
+	// startTimer();
+	// }
+	// };
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,10 +54,36 @@ public class SplashActivity extends Activity {
 		setFullScreen();
 		setContentView(R.layout.splash);
 
-		// set advertisement page background
-		LinearLayout llbg = (LinearLayout) findViewById(R.id.splash);
-		llbg.setBackgroundResource(R.drawable.ad);
+		llbg = (LinearLayout) findViewById(R.id.splash);
+		BitmapUtils bitmapUtils = new BitmapUtils(this);
+		bitmapUtils.display(llbg, "http://10.60.43.10:5000/news/mockimage");
+		// Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+		// BitmapDrawable bDrawable = new BitmapDrawable(bitmap);
+		// llbg.setBackgroundDrawable(bDrawable);
 		startTimer();
+		// new Thread(new Runnable() {
+		// @Override
+		// public void run() {
+		// MBDownloadFile httpDownLoader = new MBDownloadFile();
+		// try {
+		// int result = httpDownLoader.downFiletoDecive(urlMockimage,
+		// iamgeName);
+		// if (result == -1) {
+		// finish();
+		// System.exit(0);
+		// } else {
+		// Message msg = new Message();
+		// Bundle data = new Bundle();
+		// data.putString("dir", httpDownLoader.getDirPath());
+		// msg.setData(data);
+		// handlerBackground.sendMessage(msg);
+		// }
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		// }
+		// }).start();
 	}
 
 	public void startTimer() {
@@ -48,16 +105,16 @@ public class SplashActivity extends Activity {
 
 	private void openNextpage() {
 		Intent intent = new Intent(this, LoginActivity.class);
-		startActivity(intent);		
+		startActivity(intent);
 		this.finish();
 	}
 
-	private void setFullScreen(){
+	private void setFullScreen() {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,   
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS); 
+
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+				WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 	}
 
 	@Override
