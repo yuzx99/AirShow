fb66c29bf2e10b77183b122080f385922819909d
yuzx99 <xiaji0909@gmail.com>
2014-12-10 23:07:11 +0800

    add circled bitmap function

--- src/com/mircobox/airshow/ProfileActivity.java
+++ src/com/mircobox/airshow/ProfileActivity.java
@@ -19,7 +19,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.Bundle;
@@ -173,12 +180,45 @@ public class ProfileActivity extends Activity {
 			try {
 				Bitmap bitmap = BitmapFactory.decodeStream(cr
 						.openInputStream(uri));
-				ivPhoto.setImageBitmap(bitmap);
+				ivPhoto.setImageBitmap(getCroppedBitmap(bitmap, 75));
 			} catch (FileNotFoundException e) {
 				Log.e("Exception", e.getMessage(), e);
 			}
 		}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	/**
+	 * 
+	 * @param bmp
+	 * @param radius
+	 * @return circled bitmap
+	 */
+	public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
+		Bitmap sbmp;
+		if (bmp.getWidth() != radius || bmp.getHeight() != radius)
+			sbmp = Bitmap.createScaledBitmap(bmp, radius, radius, false);
+		else
+			sbmp = bmp;
+		Bitmap output = Bitmap.createBitmap(sbmp.getWidth(), sbmp.getHeight(),
+				Config.ARGB_8888);
+		Canvas canvas = new Canvas(output);
+
+		final int color = 0xffa19774;
+		final Paint paint = new Paint();
+		final Rect rect = new Rect(0, 0, sbmp.getWidth(), sbmp.getHeight());
+
+		paint.setAntiAlias(true);
+		paint.setFilterBitmap(true);
+		paint.setDither(true);
+		canvas.drawARGB(0, 0, 0, 0);
+		paint.setColor(Color.parseColor("#BAB399"));
+		canvas.drawCircle(sbmp.getWidth() / 2 + 0.7f,
+				sbmp.getHeight() / 2 + 0.7f, sbmp.getWidth() / 2 + 0.1f, paint);
+		paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+		canvas.drawBitmap(sbmp, rect, rect, paint);
+
+		return output;
 	}
 
 	private static Bitmap getImageThumbnail(String imagePath, int width,
@@ -189,10 +229,10 @@ public class ProfileActivity extends Activity {
 		options.inPurgeable = true;
 		options.inInputShareable = true;
 		options.inJustDecodeBounds = true;
-		// ��ȡ���ͼƬ�Ŀ�͸ߣ�ע��˴���bitmapΪnull
+		// 获取这个图片的宽和高，注意此处的bitmap为null
 		bitmap = BitmapFactory.decodeFile(imagePath, options);
-		options.inJustDecodeBounds = false; // ��Ϊ false
-		// �������ű�
+		options.inJustDecodeBounds = false; // 设为 false
+		// 计算缩放比
 		int h = options.outHeight;
 		int w = options.outWidth;
 		int beWidth = w / width;
@@ -207,10 +247,10 @@ public class ProfileActivity extends Activity {
 			be = 1;
 		}
 		options.inSampleSize = be;
-		// ���¶���ͼƬ����ȡ���ź��bitmap��ע�����Ҫ��options.inJustDecodeBounds ��Ϊ
+		// 重新读入图片，读取缩放后的bitmap，注意这次要把options.inJustDecodeBounds 设为 false
 		// false
 		bitmap = BitmapFactory.decodeFile(imagePath, options);
-		// ����ThumbnailUtils����������ͼ������Ҫָ��Ҫ�����ĸ�Bitmap����
+		// 利用ThumbnailUtils来创建缩略图，这里要指定要缩放哪个Bitmap对象
 		bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height,
 				ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
 		return bitmap;
