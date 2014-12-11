cfb16b38f200cdec431980b7a808903abff22dce
yuzx99 <xiaji0909@gmail.com>
2014-12-11 23:14:21 +0800

    update circle image method

--- src/com/mircobox/airshow/ProfileActivity.java
+++ src/com/mircobox/airshow/ProfileActivity.java
@@ -1,7 +1,10 @@
 package com.mircobox.airshow;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.json.JSONException;
@@ -10,14 +13,20 @@ import org.json.JSONObject;
 import com.lidroid.xutils.BitmapUtils;
 import com.microbox.model.UpdateProfileMolelThread;
 import com.mircobox.config.ApiUrlConfig;
+import com.mircobox.imageservice.CropOption;
+import com.mircobox.imageservice.CropOptionAdapter;
 import com.mircobox.util.MBHttpUtils;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
@@ -54,6 +63,12 @@ public class ProfileActivity extends Activity {
 
 	private long waitTime = 3000;
 	private long touchTime = 0;
+
+	private static final int PICK_FROM_CAMERA = 1;
+	private static final int CROP_FROM_CAMERA = 2;
+	private static final int PICK_FROM_FILE = 3;
+
+	private Uri mImageCaptureUri;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -133,10 +148,18 @@ public class ProfileActivity extends Activity {
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
+				// intent.setType("image/*");
+				// intent.setAction(Intent.ACTION_GET_CONTENT);
+				// startActivityForResult(Intent.createChooser(
+				// intent, "Complete action using"),
+				// PICK_FROM_FILE);
 				Intent intent = new Intent();
 				intent.setType("image/*");
 				intent.setAction(Intent.ACTION_GET_CONTENT);
-				startActivityForResult(intent, 1);
+				// startActivityForResult(intent, 1);
+				startActivityForResult(
+						Intent.createChooser(intent, "Complete action using"),
+						PICK_FROM_FILE);
 			}
 		});
 		ivPhoto = (ImageView) findViewById(R.id.ivPhoto);
@@ -173,19 +196,128 @@ public class ProfileActivity extends Activity {
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// if (resultCode == RESULT_OK) {
+		// Uri uri = data.getData();
+		// Log.e("uri", uri.toString());
+		// ContentResolver cr = this.getContentResolver();
+		// try {
+		// Bitmap bitmap = BitmapFactory.decodeStream(cr
+		// .openInputStream(uri));
+		// ivPhoto.setImageBitmap(getCroppedBitmap(bitmap, 75));
+		// } catch (FileNotFoundException e) {
+		// Log.e("Exception", e.getMessage(), e);
+		// }
+		// }
 		if (resultCode == RESULT_OK) {
-			Uri uri = data.getData();
-			Log.e("uri", uri.toString());
-			ContentResolver cr = this.getContentResolver();
-			try {
-				Bitmap bitmap = BitmapFactory.decodeStream(cr
-						.openInputStream(uri));
-				ivPhoto.setImageBitmap(getCroppedBitmap(bitmap, 75));
-			} catch (FileNotFoundException e) {
-				Log.e("Exception", e.getMessage(), e);
+			switch (requestCode) {
+			case PICK_FROM_FILE:
+				mImageCaptureUri = data.getData();
+				doCrop();
+				break;
+
+			case CROP_FROM_CAMERA:
+				if (null != data) {
+					saveCutPic(data);
+				}
+				break;
+			default:
+				break;
 			}
 		}
 		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	private void doCrop() {
+		final ArrayList<CropOption> cropOptions = new ArrayList<CropOption>();
+
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setType("image/*");
+
+		List<ResolveInfo> list = getPackageManager().queryIntentActivities(
+				intent, 0);
+
+		int size = list.size();
+		if (size == 0) {
+			Toast.makeText(this, "Can not find image crop app",
+					Toast.LENGTH_SHORT).show();
+			return;
+		} else {
+			intent.setData(mImageCaptureUri);
+			intent.putExtra("outputX", 200);
+			intent.putExtra("outputY", 200);
+			intent.putExtra("aspectX", 1);
+			intent.putExtra("aspectY", 1);
+			intent.putExtra("scale", true);
+			intent.putExtra("return-data", true);
+
+			if (size == 1) {
+				Intent i = new Intent(intent);
+				ResolveInfo res = list.get(0);
+
+				i.setComponent(new ComponentName(res.activityInfo.packageName,
+						res.activityInfo.name));
+
+				startActivityForResult(i, CROP_FROM_CAMERA);
+			} else {
+				for (ResolveInfo res : list) {
+					final CropOption co = new CropOption();
+
+					co.title = getPackageManager().getApplicationLabel(
+							res.activityInfo.applicationInfo);
+					co.icon = getPackageManager().getApplicationIcon(
+							res.activityInfo.applicationInfo);
+					co.appIntent = new Intent(intent);
+
+					co.appIntent
+							.setComponent(new ComponentName(
+									res.activityInfo.packageName,
+									res.activityInfo.name));
+
+					cropOptions.add(co);
+				}
+
+				CropOptionAdapter adapter = new CropOptionAdapter(
+						getApplicationContext(), cropOptions);
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("Choose Crop App");
+				builder.setAdapter(adapter,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								startActivityForResult(
+										cropOptions.get(item).appIntent,
+										CROP_FROM_CAMERA);
+							}
+						});
+
+				builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+					@Override
+					public void onCancel(DialogInterface dialog) {
+
+						if (mImageCaptureUri != null) {
+							getContentResolver().delete(mImageCaptureUri, null,
+									null);
+							mImageCaptureUri = null;
+						}
+					}
+				});
+
+				AlertDialog alert = builder.create();
+				alert.show();
+			}
+		}
+	}
+
+	private void saveCutPic(Intent picdata) {
+		Bundle bundle = picdata.getExtras();
+		if (null != bundle) {
+			Bitmap mBitmap = bundle.getParcelable("data");
+			ivPhoto.setImageBitmap(getCroppedBitmap(mBitmap,mBitmap.getWidth()));
+		}
+		File f = new File(mImageCaptureUri.getPath());
+		if (f.exists()) {
+			f.delete();
+		}
 	}
 
 	/**
