22f789a729badf0fc058b90e4d32f8448b97e5f0
yuzx99 <xiaji0909@gmail.com>
2014-12-13 22:06:03 +0800

    modify some layouts

--- src/com/mircobox/airshow/ProfileActivity.java
+++ src/com/mircobox/airshow/ProfileActivity.java
@@ -15,6 +15,7 @@ import com.microbox.model.UpdateProfileMolelThread;
 import com.mircobox.config.ApiUrlConfig;
 import com.mircobox.imageservice.CropOption;
 import com.mircobox.imageservice.CropOptionAdapter;
+import com.mircobox.imageservice.ImageService;
 import com.mircobox.util.MBHttpUtils;
 
 import android.app.Activity;
@@ -314,7 +315,7 @@ public class ProfileActivity extends Activity {
 		Bundle bundle = picdata.getExtras();
 		if (null != bundle) {
 			Bitmap mBitmap = bundle.getParcelable("data");
-			ivPhoto.setImageBitmap(getCroppedBitmap(mBitmap,mBitmap.getWidth()));
+			ivPhoto.setImageBitmap(ImageService.getCroppedBitmap(mBitmap,mBitmap.getWidth()));
 		}
 		File f = new File(mImageCaptureUri.getPath());
 		if (f.exists()) {
@@ -322,74 +323,7 @@ public class ProfileActivity extends Activity {
 		}
 	}
 
-	/**
-	 * 
-	 * @param bmp
-	 * @param radius
-	 * @return circled bitmap
-	 */
-	public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
-		Bitmap sbmp;
-		if (bmp.getWidth() != radius || bmp.getHeight() != radius)
-			sbmp = Bitmap.createScaledBitmap(bmp, radius, radius, false);
-		else
-			sbmp = bmp;
-		Bitmap output = Bitmap.createBitmap(sbmp.getWidth(), sbmp.getHeight(),
-				Config.ARGB_8888);
-		Canvas canvas = new Canvas(output);
-
-		final int color = 0xffa19774;
-		final Paint paint = new Paint();
-		final Rect rect = new Rect(0, 0, sbmp.getWidth(), sbmp.getHeight());
-
-		paint.setAntiAlias(true);
-		paint.setFilterBitmap(true);
-		paint.setDither(true);
-		canvas.drawARGB(0, 0, 0, 0);
-		paint.setColor(Color.parseColor("#BAB399"));
-		canvas.drawCircle(sbmp.getWidth() / 2 + 0.7f,
-				sbmp.getHeight() / 2 + 0.7f, sbmp.getWidth() / 2 + 0.1f, paint);
-		paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
-		canvas.drawBitmap(sbmp, rect, rect, paint);
-
-		return output;
-	}
-
-	private static Bitmap getImageThumbnail(String imagePath, int width,
-			int height) {
-		Bitmap bitmap = null;
-		BitmapFactory.Options options = new BitmapFactory.Options();
-		options.inPreferredConfig = Bitmap.Config.ARGB_4444;
-		options.inPurgeable = true;
-		options.inInputShareable = true;
-		options.inJustDecodeBounds = true;
-		// 获取这个图片的宽和高，注意此处的bitmap为null
-		bitmap = BitmapFactory.decodeFile(imagePath, options);
-		options.inJustDecodeBounds = false; // 设为 false
-		// 计算缩放比
-		int h = options.outHeight;
-		int w = options.outWidth;
-		int beWidth = w / width;
-		int beHeight = h / height;
-		int be = 1;
-		if (beWidth < beHeight) {
-			be = beWidth;
-		} else {
-			be = beHeight;
-		}
-		if (be <= 0) {
-			be = 1;
-		}
-		options.inSampleSize = be;
-		// 重新读入图片，读取缩放后的bitmap，注意这次要把options.inJustDecodeBounds 设为 false
-		// false
-		bitmap = BitmapFactory.decodeFile(imagePath, options);
-		// 利用ThumbnailUtils来创建缩略图，这里要指定要缩放哪个Bitmap对象
-		bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height,
-				ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
-		return bitmap;
-	}
-
+	
 	@Override
 	public void onBackPressed() {
 		long currentTime = System.currentTimeMillis();
