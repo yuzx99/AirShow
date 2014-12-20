32233a0127fcda5b1625eca6c2756b7f814b7a27
kellerwang <frank092806@gmail.com>
2014-12-20 16:39:04 +0800

    show exhibitaion info

--- src/com/microbox/exhibition/IntroductionFragment.java
+++ src/com/microbox/exhibition/IntroductionFragment.java
@@ -1,16 +1,30 @@
 package com.microbox.exhibition;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.microbox.config.ApiUrlConfig;
+import com.microbox.model.HttpGetJsonModelThread;
 import com.mircobox.airshow.R;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnTouchListener;
+import android.webkit.WebView;
+import android.webkit.WebSettings.LayoutAlgorithm;
+import android.widget.ListView;
 
 public class IntroductionFragment extends Fragment {
-	public  Fragment newInstance(Context context) {
+	private WebView wbContent;
+
+	public Fragment newInstance(Context context) {
 		IntroductionFragment f = new IntroductionFragment();
 		return f;
 	}
@@ -22,4 +36,58 @@ public class IntroductionFragment extends Fragment {
 				null);
 		return root;
 	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		initWebView();
+	}
+
+	Handler handlerContent = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			Bundle data = msg.getData();
+			String result = data.getString("result");
+			try {
+				JSONObject joObject = new JSONObject(result);
+				String content = joObject.getString("intro_content");
+				wbContent.loadDataWithBaseURL(null, content, "text/html",
+						"utf-8", null);
+				wbContent.setVerticalScrollBarEnabled(false);
+				wbContent.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+				wbContent.setScrollContainer(false);
+				wbContent.setScrollbarFadingEnabled(false);
+				wbContent.getSettings().setLayoutAlgorithm(
+						LayoutAlgorithm.SINGLE_COLUMN);
+				wbContent.getSettings().setDefaultTextEncodingName("UTF-8");
+				wbContent.getSettings().setSupportZoom(false);
+
+				wbContent.getSettings().setBuiltInZoomControls(true);
+				wbContent.getSettings().setDisplayZoomControls(false);
+				wbContent.setOnTouchListener(new OnTouchListener() {
+
+					@Override
+					public boolean onTouch(View arg0, MotionEvent arg1) {
+						// TODO Auto-generated method stub
+
+						return false;
+					}
+				});
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	};
+
+	private void initWebView() {
+		// TODO Auto-generated method stub
+		wbContent = (WebView) getView().findViewById(R.id.exhi_info);
+		HttpGetJsonModelThread hgjmt = new HttpGetJsonModelThread(
+				handlerContent, ApiUrlConfig.URL_CONFERENCE_CONTENT);
+		hgjmt.start();
+	}
+
 }
